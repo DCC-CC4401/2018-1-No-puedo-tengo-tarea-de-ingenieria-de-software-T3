a743a07eefd3b6b205f4971a3844c1209ed356fc
Mortup <g.uribemontero@gmail.com>
2018-05-05 14:54:45 -0300

    Primera iteracion de pendientes y pedidos para el administrador.

--- reservasCEI/reservasApp/views.py
+++ reservasCEI/reservasApp/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+	return HttpResponse("Indice de la pagina. Esto es lo primero que los usuarios ven.")
+
+def adminPendientes(request):
+	return render(request, 'reservasApp/adminPendientes.html')
+
+
--- reservasCEI/reservasCEI/settings.py
+++ reservasCEI/reservasCEI/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'reservasApp.apps.ReservasappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
