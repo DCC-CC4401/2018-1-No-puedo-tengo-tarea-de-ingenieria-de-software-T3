bdafc8a5afab781f619c399bde01a3587dce86be
zsamora <zsamora@ug.uchile.cl>
2018-05-09 18:25:11 -0300

    cambio en urls y v1 del calendario

--- reservasCEI/reservasApp/models.py
+++ reservasCEI/reservasApp/models.py
@@ -5,35 +5,36 @@ from django.db import models
 class Articulo (models.Model):
     nombre = models.CharField(max_length=200)
     descripcion = models.TextField()
-    estado = models.IntegerField() # 0 no disponible, 1 disponible, 2 en reparación, otros?
+    estado = models.IntegerField() # 0 en reparación, 1 disponible, 2 en préstamo (Cambiar por listas)
     def __str__(self):
         return self.nombre + ' - ' + self.descripcion
 
 class Espacio (models.Model):
     nombre = models.CharField(max_length=200)
+    capacidad = models.IntegerField(default=0)
     descripcion = models.TextField()
-    estado = models.IntegerField() # 0 no disponible, 1 disponible, 2 en reparación, otros?
+    estado = models.IntegerField(default=1) # 0 en reparación, 1 disponible, 2 en préstamo (Cambiar por listas)
     def __str__(self):
         return self.nombre + ' - ' + self.descripcion
 
 class FotoArticulo (models.Model):
-    id_articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE)
-    #foto = models.ImageField()
+    articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE)
+    ruta = models.ImageField(blank=True, null=True)
 
 class FotoEspacio (models.Model):
-    id_espacio = models.ForeignKey(Espacio, on_delete=models.CASCADE)
-    #foto = models.ImageField()
+    espacio = models.ForeignKey(Espacio, on_delete=models.CASCADE)
+    ruta = models.ImageField(blank=True, null=True)
 
 class ReservaArticulo (models.Model):
-    id_articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE)
-    fecha_inicial = models.DateTimeField()
-    fecha_final = models.DateTimeField()
+    articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE)
+    fecha_inicial = models.DateTimeField(blank=True, null=True)
+    fecha_final = models.DateTimeField(blank=True, null=True)
     #id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     estado = models.IntegerField() # 0 rechazada, 1 aprobada, 2 pendiente
 
 class ReservaEspacio (models.Model):
-    id_espacio = models.ForeignKey(Espacio, on_delete=models.CASCADE)
-    fecha_inicial = models.DateTimeField()
-    fecha_final = models.DateTimeField()
+    espacio = models.ForeignKey(Espacio, on_delete=models.CASCADE)
+    fecha_inicial = models.DateTimeField(blank=True, null=True)
+    fecha_final = models.DateTimeField(blank=True, null=True)
     #id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     estado = models.IntegerField() # 0 rechazada, 1 aprobada, 2 pendiente
--- reservasCEI/reservasApp/views.py
+++ reservasCEI/reservasApp/views.py
@@ -1,11 +1,26 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 def index(request):
 	return HttpResponse("Indice de la pagina. Esto es lo primero que los usuarios ven.")
 
-def adminPendientes(request):
-	return render(request, 'reservasApp/adminPendientes.html')
+def espacios(request, espacio_id=1):
+	espacio = Espacio.objects.get(id=espacio_id)
+	espacios_total = Espacio.objects.all()
+	context = {'espacio': espacio, 'espacios_total': espacios_total}
+	return render(request, 'reservasApp/adminPendientes.html', context)
+
+def cambiarespacio(request):
+	espacio_selecc = Espacio.objects.get(id=request.POST['espacio_selec'])
+	espacios_total = Espacio.objects.all()
+	context = {'espacio': espacio_selecc, 'espacios_total': espacios_total}
+	return render(request, 'reservasApp/adminPendientes.html', context)
+
+def fichaEspacio(request,espacio_id):
+	espacio = get_object_or_404(Espacio, id=espacio_id)
+	return render(request,'reservasApp/fichaEspacio.html',{'espacio': espacio})
 
 def listaArticulos(request):
 	return render(request, 'reservasApp/listaArticulos.html')
@@ -16,6 +31,3 @@ def listArtAvan(request):
 '''definir'''
 def buscar(request):
 	return "hola"
-
-
-
