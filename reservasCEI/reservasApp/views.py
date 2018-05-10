d5bff09fc3215f6664c5ecea98f516dcb2fb2a18
Carolina Contreras <ccre.caro@gmail.com>
2018-05-09 22:13:32 -0300

    Se corrige error en main.css

--- reservasCEI/reservasApp/views.py
+++ reservasCEI/reservasApp/views.py
@@ -3,24 +3,29 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import *
 
+
 def index(request):
     return HttpResponse("Indice de la pagina. Esto es lo primero que los usuarios ven.")
 
+
 def espacios(request, espacio_id=1):
-	espacio = Espacio.objects.get(id=espacio_id)
-	espacios_total = Espacio.objects.all()
-	context = {'espacio': espacio, 'espacios_total': espacios_total}
-	return render(request, 'reservasApp/adminPendientes.html', context)
+    espacio = Espacio.objects.get(id=espacio_id)
+    espacios_total = Espacio.objects.all()
+    context = {'espacio': espacio, 'espacios_total': espacios_total}
+    return render(request, 'reservasApp/adminPendientes.html', context)
+
 
 def cambiarespacio(request):
-	espacio_selecc = Espacio.objects.get(id=request.POST['espacio_selec'])
-	espacios_total = Espacio.objects.all()
-	context = {'espacio': espacio_selecc, 'espacios_total': espacios_total}
-	return render(request, 'reservasApp/adminPendientes.html', context)
+    espacio_selecc = Espacio.objects.get(id=request.POST['espacio_selec'])
+    espacios_total = Espacio.objects.all()
+    context = {'espacio': espacio_selecc, 'espacios_total': espacios_total}
+    return render(request, 'reservasApp/adminPendientes.html', context)
 
-def fichaEspacio(request,espacio_id):
-	espacio = get_object_or_404(Espacio, id=espacio_id)
-	return render(request,'reservasApp/fichaEspacio.html',{'espacio': espacio})
+
+def fichaEspacio(request, espacio_id):
+    espacio = get_object_or_404(Espacio, id=espacio_id)
+    return render(request, 'reservasApp/fichaEspacio.html', {'espacio': espacio})
+
 
 def listaArticulos(request):
     return render(request, 'reservasApp/listaArticulos.html')
@@ -30,12 +35,8 @@ def busquedaAvanzada(request):
     return render(request, 'reservasApp/busquedaAvanzada.html')
 
 
-
 '''definir'''
 
 
 def buscar(request):
     return "hola"
-
-
-
