b87b91611af4641f68406c417328c89cd6593e97
zsamora <zsamora@ug.uchile.cl>
2018-05-10 00:47:49 -0300

    Agrega css borrado y urls

--- reservasCEI/reservasApp/urls.py
+++ reservasCEI/reservasApp/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.conf import settings
+from django.views.static import serve
 
 from . import views
 
+app_name = 'reservasApp'
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('administrar/pendientes', views.adminPendientes, name='resPend'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^espacios/$', views.espacios, name='espacios'),
+    re_path(r'^espacios/(?P<espacio_id>[0-9]+)$', views.fichaEspacio, name='fichaEspacio'),
+    re_path(r'^cambiarespacio/$', views.cambiarespacio, name='cambiarespacio'),
     path('articulos', views.listaArticulos, name = 'listaArt'),
     path('busquedaAvanzada', views.busquedaAvanzada)
 ]
