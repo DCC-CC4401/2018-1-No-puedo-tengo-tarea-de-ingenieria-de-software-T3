07947137b052fcd0277b1e30183b818758117984
zsamora <zsamora@ug.uchile.cl>
2018-05-18 11:40:30 -0400

    actualizacion de modelos y creacion dereservas

--- reservasCEI/reservasApp/models.py
+++ reservasCEI/reservasApp/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 from datetime import *
-from django.core.exceptions import ValidationError
-
-def validar_rangoinicial(value):
-    if value < 9 or value > 17:
-        raise ValidationError(u'%s imposible asignar esta fecha inicial' % value)
-
-def validar_rangofinal(value):
-    if value < 10 or value > 18:
-        raise ValidationError(u'%s imposible asignar esta fecha final' % value)
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 ESTADO = (
     (0, 'Perdido'),
@@ -41,16 +33,19 @@ class Espacio (models.Model):
 class ReservaArticulo (models.Model):
     #id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE)
-    fecha_inicial = models.DateTimeField()
-    fecha_final = models.DateTimeField() # por defecto 1 hora mas es algo como esto:
-                                         # blank=True, default=fecha_inicial + timedelta(hours = 1))
+    fecha_inicial = models.DateField()
+    hora_inicial = models.IntegerField(validators=[MaxValueValidator(17),MinValueValidator(9)])
+    fecha_final = models.DateField()
+    hora_final = models.IntegerField(validators=[MaxValueValidator(18),MinValueValidator(10)])
     estado = models.IntegerField(choices=RESERVA_ESTADO, default=2)
 
 class ReservaEspacio (models.Model):
     #id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     espacio = models.ForeignKey(Espacio, on_delete=models.CASCADE)
-    fecha_inicial = models.DateTimeField()
-    fecha_final = models.DateTimeField()
+    fecha_inicial = models.DateField()
+    hora_inicial = models.IntegerField(validators=[MaxValueValidator(17),MinValueValidator(9)])
+    fecha_final = models.DateField()
+    hora_final = models.IntegerField(validators=[MaxValueValidator(18),MinValueValidator(10)])
     estado = models.IntegerField(choices=RESERVA_ESTADO, default=2)
     def __str__(self):
         return str(self.espacio) + ' - ' + str(self.estado)
