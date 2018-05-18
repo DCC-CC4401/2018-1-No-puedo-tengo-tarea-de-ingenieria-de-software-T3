196e0f2d3b57bade7b39e2180bdd40f66266111b
Carolina Contreras <ccre.caro@gmail.com>
2018-05-18 17:25:23 -0400

    Se agrega estado desde base de datos a lista de articulos

--- reservasCEI/reservasApp/views.py
+++ reservasCEI/reservasApp/views.py
@@ -7,20 +7,21 @@ from datetime import *
 from django.utils.timezone import utc
 from django.shortcuts import redirect
 
+
 def index(request):
     return HttpResponse("Indice de la pagina. Esto es lo primero que los usuarios ven.")
 
 
 def espacios(request, espacio_id=1, dia_actual=datetime.utcnow().replace(tzinfo=utc)):
     horario_espacio = []
-    lunes = dia_actual - timedelta(days = dia_actual.weekday())
+    lunes = dia_actual - timedelta(days=dia_actual.weekday())
     lunes_str = lunes.strftime("%d/%m")
-    viernes_str = (lunes + timedelta(days = 4)).strftime("%d/%m")
+    viernes_str = (lunes + timedelta(days=4)).strftime("%d/%m")
     semana = "Semana del " + lunes_str + " al " + viernes_str + " del " + str(dia_actual.year)
-    if (not(Espacio.objects.count())):
+    if (not (Espacio.objects.count())):
         generarHorario(espacio_id, lunes, horario_espacio)
         context = {'horario': horario_espacio, 'semana': semana}
-        return render(request, 'reservasApp/adminPendientes.html',context)
+        return render(request, 'reservasApp/adminPendientes.html', context)
     else:
         espacios_total = Espacio.objects.all()
         if (request.POST):
@@ -28,38 +29,39 @@ def espacios(request, espacio_id=1, dia_actual=datetime.utcnow().replace(tzinfo=
         espacio = Espacio.objects.get(id=espacio_id)
         generarHorario(espacio_id, lunes, horario_espacio)
         context = {'espacio': espacio, 'espacios_total': espacios_total,
-                   'horario': horario_espacio, 'semana': semana,}
+                   'horario': horario_espacio, 'semana': semana, }
         return render(request, 'reservasApp/adminPendientes.html', context)
 
+
 def generarHorario(espacio_id, dia, horario_espacio, h=9):
-    if (not(Espacio.objects.count())):
-        for h in (list(range(9,18))):
+    if (not (Espacio.objects.count())):
+        for h in (list(range(9, 18))):
             horario = []
-            horario.append(str(h)+":00 - " + str(h+1)+":00") #<td> con la hora
-            for d in (list(range(0,5))):
+            horario.append(str(h) + ":00 - " + str(h + 1) + ":00")  # <td> con la hora
+            for d in (list(range(0, 5))):
                 horario.append("")
             horario_espacio.append(horario)
     else:
         espacio = Espacio.objects.get(id=espacio_id)
         reservas_espacio = ReservaEspacio.objects.filter(espacio=espacio)
         horario = []
-        #lunes a viernes
+        # lunes a viernes
         dias = [dia,
-                dia + timedelta(days = 1),
-                dia + timedelta(days = 2),
-                dia + timedelta(days = 3),
-                dia + timedelta(days = 4)]
-        horario.append(str(h)+":00 - " + str(h+1)+":00") #<td> con la hora
+                dia + timedelta(days=1),
+                dia + timedelta(days=2),
+                dia + timedelta(days=3),
+                dia + timedelta(days=4)]
+        horario.append(str(h) + ":00 - " + str(h + 1) + ":00")  # <td> con la hora
         for d in dias:
             a = d.year
             m = d.month
             d2 = d.day
-            found = False #booleano por si encuentra reserva
+            found = False  # booleano por si encuentra reserva
             for r in reservas_espacio:
                 if (r.fecha_inicial.day == d2 and
-                    r.fecha_inicial.month == m and
-                    r.fecha_inicial.year == a and
-                    (r.fecha_inicial.hour <= h and r.fecha_final.hour > h)):
+                        r.fecha_inicial.month == m and
+                        r.fecha_inicial.year == a and
+                        (r.fecha_inicial.hour <= h and r.fecha_final.hour > h)):
                     horario.append(r.espacio)
                     found = True
                     break;
@@ -67,17 +69,52 @@ def generarHorario(espacio_id, dia, horario_espacio, h=9):
                 horario.append("")
         horario_espacio.append(horario)
         if (h < 17):
-            generarHorario(espacio_id, dia, horario_espacio,h+1)
+            generarHorario(espacio_id, dia, horario_espacio, h + 1)
+
+
+def fichaEspacio(request, espacio_id):
+    espacio = get_object_or_404(Espacio, id=espacio_id)
+    return render(request, 'reservasApp/fichaEspacio.html', {'espacio': espacio})
+
 
 def listaArticulos(request):
     return render(request, 'reservasApp/listaArticulos.html')
 
 
-def busquedaAvanzada(request):
-    return render(request, 'reservasApp/busquedaAvanzada.html')
+def busquedaAvanzada(request, articulo_id=1):
+    articulos_total = Articulo.objects.all()
 
-def busquedaSimple(request):
-    return render(request, 'reservasApp/busquedaSimple.html')
+    articulo = Articulo.objects.get(id=articulo_id)
+    articulo_nombre = Articulo.nombre
+    estado = Articulo.estado
+
+    foto_articulo = FotoArticulo.objects.get(id=articulo_id)
+
+    context = {
+        'articulo': articulo, 'articulos_total': articulos_total,
+        'articulo_nombre': articulo_nombre, 'estado': estado,
+        'foto_articulo': foto_articulo
+    }
+
+    return render(request, 'reservasApp/busquedaAvanzada.html', context)
+
+
+def busquedaSimple(request, articulo_id=1):
+    articulos_total_simple = Articulo.objects.all()
+
+    articulo_simple = Articulo.objects.get(id=articulo_id)
+    articulo_nombre_simple = Articulo.nombre
+    estado_simple = Articulo.estado
+
+    foto_articulo_simple = FotoArticulo.objects.get(id=articulo_id)
+
+    context = {
+        'articulo_simple': articulo_simple, 'articulos_total_simple': articulos_total_simple,
+        'articulo_nombre_simple': articulo_nombre_simple, 'estado_simple': estado_simple,
+        'foto_articulo_simple': foto_articulo_simple
+    }
+
+    return render(request, 'reservasApp/busquedaSimple.html', context)
 
 
 '''definir'''
@@ -85,6 +122,7 @@ def busquedaSimple(request):
 
 def buscar(request):
     return "hola"
+
 
 def reservas(request):
     return render(request, 'reservasApp/reservas.html')
