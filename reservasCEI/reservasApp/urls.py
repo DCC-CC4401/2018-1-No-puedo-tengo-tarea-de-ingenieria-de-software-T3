73bfeed0002bdb03f721a6ccf96be5694fa78c1a
Mortup <gonzalo.uribe@ug.uchile.cl>
2018-05-22 19:00:06 +0300

    Agrega nombres a las urls de busqueda

--- reservasCEI/reservasApp/urls.py
+++ reservasCEI/reservasApp/urls.py
@@ -7,9 +7,9 @@ app_name = 'reservasApp'
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
     re_path(r'^espacios/$', views.espacios, name='espacios'),
-    path('articulos', views.listaArticulos, name = 'listaArt'),
-    path('busquedaAvanzada', views.busquedaAvanzada),
-    path('busquedaSimple', views.busquedaSimple),
+    path('articulos', views.listaArticulos, name='listaArt'),
+    path('busquedaAvanzada', views.busquedaAvanzada, name='busquedaAvanzada'),
+    path('busquedaSimple', views.busquedaSimple, name='busquedaSimple'),
     re_path(r'^reservas/$', views.reservas, name='reservas'),
     path('crearUsuario', views.crearUsuario, name='crearUsuario'),
 ]
